3e7729de1177aed14aea293865d39fdee159582a
agaiffe2-iut90 <145848760+agaiffe2-iut90@users.noreply.github.com>
2023-11-29 11:15:40 +0100

    Add files via upload

---
+++ PROJET FLASK/app.py
@@ -0,0 +1,89 @@
+from flask import Flask, request, render_template, redirect, url_for, abort, flash
+
+app = Flask(__name__)
+
+
+# Sujet 3
+
+produits = [
+    {'id': 1, 'nomproduit': 'Pain Blanc', 'indiceglycémiqueproduit': '75', 'image': 'Painblanc.jpeg'},
+    {'id': 2, 'nomproduit': 'Pomme de terre cuite', 'indiceglycémiqueproduit': '85', 'image': 'PommeTerre.jpeg'},
+    {'id': 3, 'nomproduit': 'Frites', 'indiceglycémiqueproduit': '75', 'image': 'Frites.jpeg'},
+    {'id': 4, 'nomproduit': 'Brocoli', 'indiceglycémiqueproduit': '30', 'image': 'Brocoli.jpeg'},
+    {'id': 5, 'nomproduit': 'Salade', 'indiceglycémiqueproduit': '15', 'image': 'Salade.jpeg'},
+    {'id': 6, 'nomproduit': 'Tofu', 'indiceglycémiqueproduit': '15', 'image': 'Tofu.jpeg'},
+    {'id': 7, 'nomproduit': 'couscous', 'indiceglycémiqueproduit': '60', 'image': 'Couscous.jpeg'},
+]
+
+
+@app.route('/')
+def show_accueil():
+    return render_template('layout.html')
+
+@app.route('/produit/show')
+def show_produit():
+    # print(articles)
+    return render_template('/produit/show_produit.html', produits=produits)
+
+@app.route('/produit/add', methods=['GET'])
+def add_produit():
+    return render_template('produit/add_produit.html', produits=produits)
+
+@app.route('/produit/add', methods=['POST'])
+def valid_add_produit():
+    id = request.form.get('id', '')
+    nomproduit = request.form.get('nomproduit', '')
+    produit_id = request.form.get('produit_id')
+    indiceglycémiqueproduit = request.form.get('indiceglycémiqueproduit', '')
+    image = request.form.get('image', '')
+    message = u'produit modifié , id:'+id + '---- produit_id :' + produit_id + ' ----nomproduit :' + nomproduit + ' - indiceglycémiqueproduit:'+  indiceglycémiqueproduit + ' - image:' + image + + u' ------ pour le produit d identifiant :' + id
+    print(message)
+    flash(message, 'alert-success')
+    return redirect('/produit/show')
+
+@app.route('/produit/delete', methods=['GET'])
+def delete_produit():
+    id = request.args.get('id', '')
+    message=u'un produit supprimé, id : ' + id
+    flash(message, 'alert-warning')
+    return redirect('/produit/show')
+
+@app.route('/produit/edit', methods=['GET'])
+def edit_produit():
+    id = request.args.get('id', '')
+    id=int(id)
+    produits= produits[id-1]
+    return render_template('/produit/edit_produit.html', produits=produits)
+
+@app.route('/produit/edit', methods=['POST'])
+def valid_edit_produit():
+    id = request.form.get('id', '')
+    nomproduit = request.form.get('nomproduit', '')
+    produit_id = request.form.get('produit_id')
+    indiceglycémiqueproduit = request.form.get('indiceglycémiqueproduit', '')
+    image = request.form.get('image', '')
+    message = u'produit modifié , id:'+id + '---- produit_id :' + produit_id + ' ----nomproduit :' + nomproduit + ' - indiceglycémiqueproduit:'+  indiceglycémiqueproduit + ' - image:' + image + + u' ------ pour le produit d identifiant :' + id
+    print(message)  
+    flash(message, 'alert-success')
+    return redirect('/produit/show')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
